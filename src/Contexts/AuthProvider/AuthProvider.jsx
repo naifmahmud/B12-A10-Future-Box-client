4425674609eabbee84f03437eac6c11e8c74d749
naif <naifmahmud777@gmail.com>
2025-11-27 18:26:28 +0600

    Added googleSignIn in register and loagin page

--- src/Contexts/AuthProvider/AuthProvider.jsx
+++ src/Contexts/AuthProvider/AuthProvider.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { AuthContext } from '../AuthContext/AuthContext';
+import {GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import {auth} from '../../firebase/firebase.config'
+
+const googleProvider= new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
 
-    const userInfo={
+    const signInWithGoogle=()=>{
+        return signInWithPopup(auth,googleProvider);
+    }
 
+
+    const userInfo={
+        signInWithGoogle
     }
     return (
         <AuthContext value={userInfo}>
--- src/Pages/Authentication/Login.jsx
+++ src/Pages/Authentication/Login.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
-import { NavLink } from 'react-router';
+import React, { use } from 'react';
+import { NavLink, useLocation, useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+import { AuthContext } from '../../Contexts/AuthContext/AuthContext';
 
 const Login = () => {
+
+
+   const {signInWithGoogle}= use(AuthContext);
+
+    const location= useLocation();
+    const navigate= useNavigate();
+    console.log(location);
+    
+
+    const handleGoogleButton=()=>{
+        signInWithGoogle()
+        .then(result=>{
+            console.log(result.user);
+            toast.success("Account Registered successfully");
+            navigate(location.state)
+        })
+        .catch(error=>{
+            console.log(error.message);
+        })
+    }
+
+
+
+
+
+
     return (
         <div className="hero min-h-screen">
   <div className="hero-content flex-col lg:flex-row-reverse">
@@ -21,7 +49,7 @@ const Login = () => {
         </fieldset>
         </form>
         {/* Google */}
-        <button className="btn bg-white text-black border-[#e5e5e5]">
+        <button onClick={handleGoogleButton} className="btn bg-white text-black border-[#e5e5e5]">
         <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
         Login with Google
         </button>
