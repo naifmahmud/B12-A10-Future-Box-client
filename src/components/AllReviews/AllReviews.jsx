1828162803d797def076586f16ad449b9d7d17a9
naif <naifmahmud777@gmail.com>
2025-11-30 00:33:22 +0600

    Added Search Feature in mongodb using regex

--- src/components/AllReviews/AllReviews.jsx
+++ src/components/AllReviews/AllReviews.jsx
@@ -1,26 +1,64 @@
-import React, { use } from 'react';
-import { useLoaderData } from 'react-router';
-import ReviewCard from '../ReviewCard/ReviewCard';
-import { AuthContext } from '../../Contexts/AuthContext/AuthContext';
+import React, { use, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { useLoaderData } from "react-router";
+import ReviewCard from "../ReviewCard/ReviewCard";
+import { AuthContext } from "../../Contexts/AuthContext/AuthContext";
 
 const AllReviews = () => {
+  const { loading } = use(AuthContext);
 
-    const {loading}=use(AuthContext);
+  const allReview = useLoaderData();
 
-    const allReview=useLoaderData();
-    // console.log(allReview);
-    
-    if(loading){
-        return <span className="skeleton skeleton-text">Wait a sec data loading...</span>
-    }
+  const [allReviews,setReviews]= useState(allReview);
 
+  if (loading) {
     return (
-        <div className='my-10'>
-            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-                {allReview.map(reviews=><ReviewCard key={reviews._id} review={reviews}></ReviewCard>)}
-            </div>
-        </div>
+      <span className="skeleton skeleton-text">Wait a sec data loading...</span>
     );
+  }
+
+  const handleSearch=(e)=>{
+
+    e.preventDefault();
+    const searchText=e.target.search.value;
+
+    if(loading){
+        return <div>loading....</div>
+    }
+    fetch(`http://localhost:3000/search?search=${searchText}`)
+    .then(res=> res.json())
+    .then(data=>{
+        console.log(data);
+        setReviews(data)
+    })
+  }
+
+  return (
+    <div className="my-10">
+      <div>
+
+        <form onSubmit={handleSearch}>
+            <div className="searchBox my-5 mx-auto">
+          <input
+            className="border-none p-2 rounded-l-2xl"
+            type="text"
+            name="search"
+            placeholder="Search here"
+          />
+          <button className="searchButton ml-3 pl-3">
+            <FaSearch className="w-8 h-4" />
+          </button>
+        </div>
+        </form>
+
+      </div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {allReviews.map((reviews) => (
+          <ReviewCard key={reviews._id} review={reviews}></ReviewCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllReviews;
